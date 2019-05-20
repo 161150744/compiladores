e9681bb386d7e140d24836077c1edae30eb187e6
Lucas Antunes de Almeida <lucas.lukinha67@gmail.com>
2019-05-20 10:27:43 -0300

    nao tem nada

---
+++ _Source/lista.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "lista.h"
+
+struct tac* create_inst_tac(const char* res, const char* arg1, const char* op, const char* arg2){
+	
+
+
+}
+
+void print_inst_tac(FILE* out, struct tac i){
+
+	
+
+}
+
+void print_tac(FILE* out, struct node_tac * code){
+	
+
+    
+}
+
+
+void append_inst_tac(struct node_tac ** code, struct tac * inst){
+	
+
+
+}
+
+
+void cat_tac(struct node_tac ** code_a, struct node_tac ** code_b){
+	
+	
+
+}    
